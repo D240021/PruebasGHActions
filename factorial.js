3dbaf4c952182a40b16b90f2ea86db5f74bb757a
Daniel Barrantes🖥️📚 <104049620+D240021@users.noreply.github.com>
2025-01-19 13:25:59 -0600

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,6 @@
+function obtenerFactorial( numero ) {
+    return numero <= 1 ? 1 : numero * obtenerFactorial( numero - 1 );
+}
+
+module.exports = { obtenerFactorial };
+
---
+++ factorial.test.js
@@ -0,0 +1,27 @@
+const { obtenerFactorial } = require('./factorial.js');
+ 
+describe('Pruebas para la función obtenerFactorial', () => {
+    test('Factorial de 0 debe ser 1', () => {
+        expect(obtenerFactorial(0)).toBe(1);
+    });
+
+    test('Factorial de 1 debe ser 1', () => {
+        expect(obtenerFactorial(1)).toBe(1);
+    });
+
+    test('Factorial de 5 debe ser 120', () => {
+        expect(obtenerFactorial(5)).toBe(120);
+    });
+
+    test('Factorial de 10 debe ser 3628800', () => {
+        expect(obtenerFactorial(10)).toBe(3628800);
+    });
+
+    test('Factorial de números negativos debe ser 1', () => {
+        expect(obtenerFactorial(-5)).toBe(1);
+    });
+
+    // test('Factorial de 5 debe ser incorrecto deliberadamente', () => {
+    //      expect(obtenerFactorial(5)).toBe(100); 
+    //  });
+});
