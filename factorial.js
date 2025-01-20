84307e54142a74d6711fb742d2f2c066bb80f716
Daniel Barrantes <danielbarrantesq@gmail.com>
2025-01-20 17:57:50 -0600

    Se agrega error de formato

--- factorial.js
+++ factorial.js
@@ -1,7 +1,5 @@
-function obtenerFactorial( numero ) {
-    return numero <= 1 ? 1 : numero * obtenerFactorial( numero - 1 );
+function obtenerFactorial (numero) {
+  return numero <= 1 ? 1 : numero * obtenerFactorial(numero - 1)
 }
 
-module.exports = { obtenerFactorial };
-
-
+module.exports = { obtenerFactorial }
