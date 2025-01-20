78d5ecdb83bb3398faccafd0e10507e147ab55f6
Daniel Barrantes <danielbarrantesq@gmail.com>
2025-01-20 15:12:04 -0600

    Cambios index

--- factorial.js
+++ factorial.js
@@ -1,7 +1,6 @@
 function obtenerFactorial( numero ) {
     return numero <= 1 ? 1 : numero * obtenerFactorial( numero - 1 );
 }
-
 // eslint-disable-next-line no-undef
 module.exports = { obtenerFactorial };
 
