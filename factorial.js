0890bceaf5afd7873d5b0e37ef40e93b4c26f0f6
Daniel Barrantes <danielbarrantesq@gmail.com>
2025-01-20 15:26:19 -0600

    Se agrega formato standard

--- factorial.js
+++ factorial.js
@@ -1,6 +1,5 @@
-function obtenerFactorial( numero ) {
-    return numero <= 1 ? 1 : numero * obtenerFactorial( numero - 1 );
+function obtenerFactorial (numero) {
+  return numero <= 1 ? 1 : numero * obtenerFactorial(numero - 1)
 }
 // eslint-disable-next-line no-undef
-module.exports = { obtenerFactorial };
-
+module.exports = { obtenerFactorial }
